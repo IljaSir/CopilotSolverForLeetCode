8df8212e5625832853a4a6a605100c28f03e3427
Ilja Sirosh <iljasirosh@mbp-ilja.home>
2024-02-08 21:26:55 +0100

    Corrections

--- Python3/Hard/FailedToSolve/custom-sort-string_1.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def customSortString(self, order: str, s: str) -> str:
-        
-        order_dict = {c: i for i, c in enumerate(order)}
-        return "".join(sorted(s, key=lambda c: order_dict.get(c, 0)))
